bcf8d22543ea7d934912092799f956e0a66cdcaf
kodluyoruz <ensarsehitoglu25@gmail.com>
2022-01-26 21:43:04 +0300

    u

--- services/products.js
+++ services/products.js
@@ -134,7 +134,7 @@ exports.fetchIsDiscount = async(params)=>{
         },
 
         ExpressionAttributeValues: {
-             ":data": "true",
+             ":data": true,
         }
     }
     try{
